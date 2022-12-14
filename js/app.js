2d41e2b7ca21b96be34c5fc954e26ede650b6d5c
justcoddev <edisonpaulcrz@gmail.com>
2022-12-14 12:18:03 -0500

    new

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 const productos = [];
 
+function modificar() {
+  const forma = document.forms['forma'];
+  const id = forma['id'];
+  const nombre = forma['nombre'];
+  const cantidad = forma['cantidad'];
+  const precio = forma['precio'];
+
+
+  const producto = new Producto(id.value, nombre.value, cantidad.value, precio.value);
+  productos.push(producto);
+  for (let producto of productos) {
+    console.log(producto);
+  }
+  mostrar();
+
+}
+// function eliminar() {
+
+// }
+const eliminar = (producto) => {
+  // let indiceEliminar = productos.findIndex(producto => producto.id === id);
+  // productos.splice(indiceEliminar, id);
+
+  var indice = producto.indexOf(3); // obtenemos el indice
+  producto.splice(indice, 1); // 1 es la cantidad de elemento a eliminar
+
+  console.log(arreglo);
+
+  mostrar();
+}
+
 function mostrar() {
   // let id=0;
   // let nombre="";
@@ -17,8 +48,9 @@ function mostrar() {
 
     // prec=producto.precio;
 
-    texto += `<tr> <td> ${producto.idproducto}  </td> <td> ${producto.nombre}  </td><td> ${producto.cantidad}  </td><td> ${producto.precio}  </td> </tr>`;
+    texto += `<tr> <td> ${producto.idproducto}  </td> <td> ${producto.nombre}  </td><td> ${producto.cantidad}  </td><td> ${producto.precio}  </td> <td> <button class="boton" onclick="modificar()">EDITAR</button></td> <td> <button class="boton" onclick="eliminar()">ELIMINAR</button></td> </tr>`;
     console.log(tabla);
+    console.log(producto);
   }
   document.getElementById('tabla').innerHTML = texto;
   // document.getElementById('tableidproducto').innerHTML =id ;
